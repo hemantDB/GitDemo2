d31d90b6aed83885d2f85cdadccce95e6eae2697
Hemant <Hemant@gmail.com>
2021-12-05 08:12:27 +0530

    Added Spring project

---
+++ GtPoc/src/main/java/com/git/demo/GtPocApplication.java
@@ -0,0 +1,13 @@
+package com.git.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtPocApplication.class, args);
+	}
+
+}
